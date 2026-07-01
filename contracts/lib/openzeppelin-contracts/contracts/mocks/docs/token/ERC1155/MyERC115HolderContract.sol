8f05d8bfe84dd16440cf0c8f6b4092873e12f7b4
OT-P53 <OT-P53@proton.me>
2026-07-01 03:23:13 -0400

    zk

---
+++ contracts/lib/openzeppelin-contracts/contracts/mocks/docs/token/ERC1155/MyERC115HolderContract.sol
@@ -0,0 +1,7 @@
+// contracts/MyERC115HolderContract.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ERC1155Holder} from "../../../../token/ERC1155/utils/ERC1155Holder.sol";
+
+contract MyERC115HolderContract is ERC1155Holder {}
